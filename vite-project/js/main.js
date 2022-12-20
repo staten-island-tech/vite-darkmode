f14213776300755a92aded0f74dacecaa6fe3577
andreaguo <113132239+andreaguo@users.noreply.github.com>
2022-12-19 22:26:00 -0500

    .

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,16 +1,58 @@
 import '../styles/style.css';
-
-import AOS from "aos";
-AOS.init();
+// import AOS from "aos";
+// AOS.init();
 import {menu} from "./menu";
-
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//   </div>
-// `
 console.log(menu);
 
+function appetizerFilter(){
+menu.filter((el)=> el.type === "Appetizer") 
+.forEach((el)=> {
+    console.log(el.name);
+})
+};
+
+function phoFilter(){
+menu.filter((el)=> el.type === "Pho")
+.forEach((el)=> {
+    console.log(el.name);
+})
+}
+
+function sandwichFilter(){
+menu.filter((el)=> el.type === "Banh Mi")
+.forEach((el)=> {
+    console.log(el.name);
+})
+}
+
+appetizerFilter();
+phoFilter();
+sandwichFilter();
+
+const DOMSelectors={
+    btnA: document.querySelector(".btnA"),
+    btnB: document.querySelector(".btnB"),
+    btnC: document.querySelector(".btnC"),
+    appDisplay: document.querySelector(".appetizers"),
+    phoDisplay: document.querySelector(".noodleSoup"),
+    sandDisplay: document.querySelector(".sandwiches"),
+};
+
+DOMSelectors.btnA.addEventListener("click", function(){
+    const app ={};
+    app.name = menu[0,1,2].name.value;
+    app.description = menu[0,1,2].description.value;
+    app.price = menu[0,1,2].price.value;
+    html(app);
+})
+
+function html(app){
+    DOMSelectors.appDisplay.insertAdjacentHTML(
+        "afterbegin",
+        `<div class="dish-card">
+        <h3 class="dish-name">${app.name}</h3>
+        <h4 class="dish-description">${app.description}</h4>
+        <h4 class="dish-name">${app.price}</h4>
+        </div>`
+    )
+}
--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -1,53 +1,80 @@
 const menu = [
-{name: "chicken",
+{type: "Appetizer",
+name: "Cha Gio",
+description: "Vietnamese Spring Roll",
+price: 8.95,
 rating: 9.9,
 img: "link", 
 vegetarian: false
 },
 
-{name: "chicken",
+{type: "Appetizer",
+name: "Gio Cuon",
+description: "Summer Roll Shrimp, Vegetable with Rice Paper",
+price: 3.25,
 rating: 9.9,
 img: "link", 
 vegetarian: false
 },
 
-{name: "chicken",
+{type: "Appetizer",
+name: "Bi Cuon",
+description: "Shredded Pork with Vegetable Roll with Rice Paper",
+price: 2.95,
 rating: 9.9,
 img: "link", 
 vegetarian: false
 },
 
-{name: "chicken",
+{type: "Pho",
+name: "Pho Dac Biet",
+description: "Special Rice Noodle Soup with Beef, Brisket, Navel, Flank, and Tendon",
+price: 4.50,
 rating: 9.9,
 img: "link", 
 vegetarian: false
 },
 
-{name: "chicken",
+{type: "Pho",
+name: "Pho Suon Nuong",
+description: "Grilled Pork Chop on Rice Noodle Soup",
+price: 4.25,
 rating: 9.9,
 img: "link", 
 vegetarian: false
 },
 
-{name: "chicken",
+{type: "Pho",
+name: "Pho Ga Nuong",
+description: "Grilled Chicken on Rice Noodle Soup",
+price: 4.75,
 rating: 9.9,
 img: "link", 
 vegetarian: false
 },
 
-{name: "chicken",
+{type: "Banh Mi",
+name: "Bahn Mi Dac Biet",
+description: "Vietnamese Bread",
+price: 2.50,
 rating: 9.9,
 img: "link", 
 vegetarian: false
 },
 
-{name: "chicken",
+{type: "Banh Mi",
+name: "Bahn Mi Cary Ga",
+description: "Curry Chicken with Bread",
+price: 4.50,
 rating: 9.9,
 img: "link", 
 vegetarian: false
 },
 
-{name: "chicken",
+{type: "Bahn Mi",
+name: "Bahn Mi Bo Kho",
+description: "Curry Beef Stew with Bread",
+price: 4.50,
 rating: 9.9,
 img: "link", 
 vegetarian: false
