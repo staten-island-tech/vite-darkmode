f44ea4605ab90f11226bbde1f0835636c2723dc1
andreaguo <113132239+andreaguo@users.noreply.github.com>
2022-12-21 01:31:33 -0500

    committt

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -15,47 +15,15 @@ const DOMSelectors={
     appDisplay: document.querySelector(".appDiv"),
     phoDisplay: document.querySelector(".phoDiv"),
     sandDisplay: document.querySelector(".sandDiv"),
+    him: change("Appetizer"),
+    her: change("Pho"),
+    them: change("Banh Mi"),
 };
 
-function clickBtnA(){
-    DOMSelectors.btnA.addEventListener("click", function(){
-    let him = change("Appetizer");
-    console.log(him);
-    htmlHim(menu[0]);
-    htmlHim(menu[1]);
-    htmlHim(menu[2]);
-});
-}
-
-function clickBtnB(){
-DOMSelectors.btnB.addEventListener("click", function(){
-    let him = change("Pho");
-    console.log(him);
-    htmlHer(menu[3]);
-    htmlHer(menu[4]);
-    htmlHer(menu[5]);
-});
-}
-
-function clickBtnC(){
-DOMSelectors.btnC.addEventListener("click", function(){
-    let him = change("Banh");
-    console.log(him);
-    htmlThem(menu[6]);
-    htmlThem(menu[7]);
-    htmlThem(menu[8]);
-});
-}
-
-clickBtnA();
-clickBtnB();
-clickBtnC();
-   
-// DOMSelectors.btnD.addEventListener("click", function(){
-
-// })
-
-function htmlHim(dish){
+DOMSelectors.btnA.addEventListener("click", function(){
+    console.log(DOMSelectors.him);
+    const map1 = Array.from(DOMSelectors.him);
+    map1.forEach(function htmlHim(dish){
     DOMSelectors.appDisplay.insertAdjacentHTML(
         "beforeend",
         `<div class="dish-card">
@@ -64,27 +32,55 @@ function htmlHim(dish){
         <h4 class="dish-name">${dish.price}</h4>
         </div>
        `
-)}
+)})
+}); 
+// use .map to create a new array with objects in menu that have given type
+// then htmlHim that
 
-function htmlHer(dish){
+DOMSelectors.btnB.addEventListener("click", function(){
+    console.log(DOMSelectors.her);
+    const map2 = Array.from(DOMSelectors.her);
+    map2.forEach(function htmlHer(dish){
     DOMSelectors.phoDisplay.insertAdjacentHTML(
-        "beforeend",
-        `<div class="dish-card">
-        <h3 class="dish-name">${dish.name}</h3>
-        <h4 class="dish-description">${dish.description}</h4>
-        <h4 class="dish-name">${dish.price}</h4>
-        </div>
-       `
-)}
-function htmlThem(dish){
+            "beforeend",
+            `<div class="dish-card">
+            <h3 class="dish-name">${dish.name}</h3>
+            <h4 class="dish-description">${dish.description}</h4>
+            <h4 class="dish-name">${dish.price}</h4>
+            </div>
+           `
+)})
+});
+
+DOMSelectors.btnC.addEventListener("click", function(){
+    console.log(DOMSelectors.them);
+    const map3 = Array.from(DOMSelectors.them);
+    map3.forEach(function htmlHer(dish){
     DOMSelectors.sandDisplay.insertAdjacentHTML(
-        "beforeend",
-        `<div class="dish-card">
-        <h3 class="dish-name">${dish.name}</h3>
-        <h4 class="dish-description">${dish.description}</h4>
-        <h4 class="dish-name">${dish.price}</h4>
-        </div>
-       `
-)}
-//in the addeventlistener function, use map to create a new function out of the change(); with .map and in that function 
-//define the three values of each of those arrays .value (ie: let dishOne = newArray[o].value)
+            "beforeend",
+            `<div class="dish-card">
+            <h3 class="dish-name">${dish.name}</h3>
+            <h4 class="dish-description">${dish.description}</h4>
+            <h4 class="dish-name">${dish.price}</h4>
+            </div>
+            `
+    )})
+    });
+
+DOMSelectors.btnAll.addEventListener("click", function(){
+})
+
+
+function start(){
+    htmlHim(menu[0]);
+    htmlHim(menu[1]);
+    htmlHim(menu[2]);
+    htmlHer(menu[3]);
+    htmlHer(menu[4]);
+    htmlHer(menu[5]);
+    htmlThem(menu[6]);
+    htmlThem(menu[7]);
+    htmlThem(menu[8]);
+}
+
+start();
