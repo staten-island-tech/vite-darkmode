56c093b961b93731e3df30765157ccd29376bcf5
andreaguo <113132239+andreaguo@users.noreply.github.com>
2022-12-21 21:53:35 -0500

    colors

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -13,8 +13,11 @@ const DOMSelectors={
     btnC: document.querySelector(".btnC"),
     btnAll: document.querySelector(".btnAll"),
     appDisplay: document.querySelector(".appetizers"),
-    phoDisplay: document.querySelector(".phoDiv"),
-    sandDisplay: document.querySelector(".sandDiv"),
+    subAppDisplay: document.querySelector(".appDiv"),
+    phoDisplay: document.querySelector(".noodleSoup"),
+    subPhoDisplay: document.querySelector(".phoDiv"),
+    sandDisplay: document.querySelector(".sandwiches"),
+    subSandDisplay: document.querySelector(".sandDiv"),
     him: change("Appetizer"),
     her: change("Pho"),
     them: change("Banh Mi"),
@@ -22,20 +25,21 @@ const DOMSelectors={
 
 DOMSelectors.btnA.addEventListener("click", function(){
     console.log(DOMSelectors.him);
-    const map1 = Array.from(DOMSelectors.him);
-    map1.forEach(function htmlHim(dish){
     DOMSelectors.appDisplay.insertAdjacentHTML(
-        "beforeend",
-        `    <div class="subhead">
+        "afterbegin",
+        `<div class="subhead">
         <h3 class="hero">appetizers!</h3>
         <h4 class="hero">here are some good starters</h4>
-      </div>
-        <div class="appDiv">
-        <div class="dish-card">
+      </div>`
+    )
+    const map1 = Array.from(DOMSelectors.him);
+    map1.forEach(function htmlHim(dish){
+    DOMSelectors.subAppDisplay.insertAdjacentHTML(
+        "beforeend",
+        `<div class="dish-card">
         <h3 class="dish-name">${dish.name}</h3>
         <h4 class="dish-description">${dish.description}</h4>
         <h4 class="dish-name">${dish.price}</h4>
-        </div>
         </div>
        `
 )})
@@ -45,9 +49,16 @@ DOMSelectors.btnA.addEventListener("click", function(){
 
 DOMSelectors.btnB.addEventListener("click", function(){
     console.log(DOMSelectors.her);
+    DOMSelectors.phoDisplay.insertAdjacentHTML(
+        "afterbegin",
+        `<div class="subhead">
+        <h3 class="hero">pho!</h3>
+        <h4 class="hero">the best part</h4>
+      </div>`
+    )
     const map2 = Array.from(DOMSelectors.her);
     map2.forEach(function htmlHer(dish){
-    DOMSelectors.phoDisplay.insertAdjacentHTML(
+    DOMSelectors.subPhoDisplay.insertAdjacentHTML(
             "beforeend",
             `<div class="dish-card">
             <h3 class="dish-name">${dish.name}</h3>
@@ -60,9 +71,16 @@ DOMSelectors.btnB.addEventListener("click", function(){
 
 DOMSelectors.btnC.addEventListener("click", function(){
     console.log(DOMSelectors.them);
+    DOMSelectors.sandDisplay.insertAdjacentHTML(
+        "afterbegin",
+        `<div class="subhead">
+        <h3 class="hero">sandwiches!</h3>
+        <h4 class="hero">it gets even better</h4>
+      </div>`
+    )
     const map3 = Array.from(DOMSelectors.them);
     map3.forEach(function htmlHer(dish){
-    DOMSelectors.sandDisplay.insertAdjacentHTML(
+    DOMSelectors.subSandDisplay.insertAdjacentHTML(
             "beforeend",
             `<div class="dish-card">
             <h3 class="dish-name">${dish.name}</h3>
@@ -73,20 +91,20 @@ DOMSelectors.btnC.addEventListener("click", function(){
     )})
     });
 
-DOMSelectors.btnAll.addEventListener("click", function(){
-})
+DOMSelectors.btnAll.addEventListener("click", start());
 
+document.querySelector(".color").addEventListener("click", function () {
+    if (document.body.classList.contains("cool")) {
+      document.body.classList.add("warm");
+      document.body.classList.remove("cool");
+    } else {
+      document.body.classList.add("cool");
+      document.body.classList.remove("warm");
+    }
+});
 
 function start(){
-    htmlHim(menu[0]);
-    htmlHim(menu[1]);
-    htmlHim(menu[2]);
-    htmlHer(menu[3]);
-    htmlHer(menu[4]);
-    htmlHer(menu[5]);
-    htmlThem(menu[6]);
-    htmlThem(menu[7]);
-    htmlThem(menu[8]);
-}
+    
+};
 
 start();
