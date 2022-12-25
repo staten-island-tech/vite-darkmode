f6fdab50615b17a93b89530f6ba958ffc8713c11
andreaguo <113132239+andreaguo@users.noreply.github.com>
2022-12-25 13:20:32 -0500

    yo

--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -4,7 +4,7 @@ name: "Cha Gio",
 description: "Vietnamese Spring Roll",
 price: 8.95,
 rating: 9.9,
-img: "",
+img: "https://thewoksoflife.com/wp-content/uploads/2020/08/cha-gio-vietnamese-fried-spring-rolls-15.jpg",
 vegetarian: false
 },
 
@@ -22,7 +22,7 @@ name: "Bi Cuon",
 description: "Shredded Pork with Vegetable Roll with Rice Paper",
 price: 2.95,
 rating: 9.9,
-img: "", 
+img: "https://i0.wp.com/www.wokandkin.com/wp-content/uploads/2020/05/Bi-Cuon-saved-for-web.png?resize=600%2C900&ssl=1", 
 vegetarian: false
 },
 
@@ -31,7 +31,7 @@ name: "Pho Dac Biet",
 description: "Special Rice Noodle Soup with Beef, Brisket, Navel, Flank, and Tendon",
 price: 4.50,
 rating: 9.9,
-img: "", 
+img: "https://omnivorescookbook.com/wp-content/uploads/2016/01/1511_Easy-Vietnamese-Pho-Noodle-Soup_002-1.jpg", 
 vegetarian: false
 },
 
@@ -40,7 +40,7 @@ name: "Pho Suon Nuong",
 description: "Grilled Pork Chop on Rice Noodle Soup",
 price: 4.25,
 rating: 9.9,
-img: "", 
+img: "https://beyondthenoms.com/wp-content/uploads/2021/01/Vietnamese-Grilled-Lemongrass-Pork-3.jpg", 
 vegetarian: false
 },
 
@@ -49,7 +49,7 @@ name: "Pho Ga Nuong",
 description: "Grilled Chicken on Rice Noodle Soup",
 price: 4.75,
 rating: 9.9,
-img: "", 
+img: "https://www.recipetineats.com/wp-content/uploads/2020/05/Chicken-Pho_2.jpg", 
 vegetarian: false
 },
 
@@ -58,7 +58,7 @@ name: "Bahn Mi Dac Biet",
 description: "Vietnamese Bread",
 price: 2.50,
 rating: 9.9,
-img: "", 
+img: "https://omnivorescookbook.com/wp-content/uploads/2020/03/1912_Leftover-Ham-Banh-Mi_003.jpg", 
 vegetarian: false
 },
 
@@ -67,7 +67,7 @@ name: "Bahn Mi Cary Ga",
 description: "Curry Chicken with Bread",
 price: 4.50,
 rating: 9.9,
-img: "", 
+img: "https://www.wandercooks.com/wp-content/uploads/2022/01/vietnamese-chicken-curry-5-683x1024.jpg", 
 vegetarian: false
 },
 
@@ -76,7 +76,7 @@ name: "Bahn Mi Bo Kho",
 description: "Curry Beef Stew with Bread",
 price: 4.50,
 rating: 9.9,
-img: "", 
+img: "https://i0.wp.com/www.wokandkin.com/wp-content/uploads/2021/11/Bo-Kho-Bread-saved-for-web-1200px.png?fit=1200%2C1800&ssl=1", 
 vegetarian: false
 },
 ]
